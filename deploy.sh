1d40f0a2417c4aa5ff9176380fef9587c6e60e1e
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-05-23 14:20:10 +0300

    kill port 8080 process

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# removing key, scp want to copy it again in each run
+rm -f mykey.pem
+# kill the process that hold 8080 port
 kill -9 "$(sudo lsof -t -i :8080)"
-
+# run the app
 python3 app.py &
