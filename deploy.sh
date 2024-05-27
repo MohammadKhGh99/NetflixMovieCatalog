0d6a16c5a3debe57b9d7eba3bd99578600621e20
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-05-27 13:42:37 +0300

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # removing key, scp want to copy it again in each run
 rm -f mykey.pem
 # kill the process that hold 8080 port
-kill -9 "$(sudo lsof -t -i :8080)"
+PID=$(sudo lsof -t -i :8080)
+if [ $PID != "" ]; then
+  kill -9 "$(sudo lsof -t -i :8080)"
+fi
 # run the app
 nohup python3 app.py > /dev/null 2>&1 &
